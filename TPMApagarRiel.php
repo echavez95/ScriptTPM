b5e6a66710468f81e99c5635ec8e6992563d20ba
Eliezer Chavez <eliezer.chavez@landau.com>
2019-06-03 10:35:24 -0600

    agregar script nuevo

---
+++ TPMApagarRiel.php
@@ -0,0 +1,5 @@
+﻿<?php
+$retraso = $_GET['retraso'];
+$comando = "echo raspberry | sudo -S python /var/www/html/scripts/TPMApagarRiel.py ".$retraso;
+system($comando);
+?>
---
+++ TPMApagarRiel.py
@@ -0,0 +1,17 @@
+﻿import RPi.GPIO as GPIO
+import sys
+from time import sleep
+retraso=int(sys.argv[1])
+retrasoSegundos = retraso * 60
+pin = 21
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin,GPIO.OUT)
+
+#    print "Encender"
+GPIO.output(pin,False)
+sleep(retrasoSegundos)
+#    print "Apagar"
+GPIO.output(pin,True)
+
+exit(0)
